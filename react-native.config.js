1503aab4cf95c3f03838ea3c68b0c4a9b2c0350a
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2019-11-27 09:55:54 +0100

    Fixed autolink configuration

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  dependencies: {
-    "@keeex/react-native-threads": {
-      platforms: {
-        android: null,
-      },
-    },
+  platforms: {
+    android: null,
   },
 };
